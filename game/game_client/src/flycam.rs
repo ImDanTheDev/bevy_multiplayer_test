de807904ed5ea1ac6d159c2b4cfdea97c18fa746
ImDanTheDev <dmott248@gmail.com>
2022-08-15 18:55:13 -0400

    Added flycam to game client

---
+++ game/game_client/src/flycam.rs
@@ -0,0 +1,57 @@
+use bevy::{
+    input::mouse::MouseMotion,
+    prelude::{
+        Camera, EventReader, Input, KeyCode, Local, MouseButton, Quat, Query, Res, Transform, Vec3,
+        With,
+    },
+    time::Time,
+};
+
+pub fn flycam_movement(
+    time: Res<Time>,
+    input: Res<Input<KeyCode>>,
+    buttons: Res<Input<MouseButton>>,
+    mut motion_evr: EventReader<MouseMotion>,
+    mut query: Query<&mut Transform, With<Camera>>,
+    mut px: Local<f32>,
+    mut py: Local<f32>,
+) {
+    if buttons.pressed(MouseButton::Right) {
+        for ev in motion_evr.iter() {
+            *py -= ev.delta.x * 0.1;
+            *px -= ev.delta.y * 0.1;
+        }
+    }
+
+    let x_input = match (input.pressed(KeyCode::A), input.pressed(KeyCode::D)) {
+        (true, false) => -1.0,
+        (false, true) => 1.0,
+        _ => 0.0,
+    };
+
+    let z_input = match (input.pressed(KeyCode::W), input.pressed(KeyCode::S)) {
+        (true, false) => -1.0,
+        (false, true) => 1.0,
+        _ => 0.0,
+    };
+
+    let y_input = match (
+        input.pressed(KeyCode::Space),
+        input.pressed(KeyCode::LControl),
+    ) {
+        (true, false) => 1.0,
+        (false, true) => -1.0,
+        _ => 0.0,
+    };
+
+    let look_rotation = Quat::from_axis_angle(Vec3::Y, py.to_radians())
+        * Quat::from_axis_angle(Vec3::X, px.to_radians());
+    let input_direction = Vec3::new(x_input, y_input, z_input).normalize_or_zero();
+    let movement_direction = look_rotation * input_direction;
+
+    for mut transform in query.iter_mut() {
+        transform.rotation = look_rotation;
+
+        transform.translation += movement_direction * 4.0 * time.delta_seconds();
+    }
+}
--- game/game_client/src/main.rs
+++ game/game_client/src/main.rs
@@ -1,9 +1,59 @@
-use bevy::{prelude::App, DefaultPlugins};
+mod flycam;
+use bevy::{
+    prelude::{
+        shape::Cube, App, Assets, Camera3dBundle, Color, Commands, DirectionalLight,
+        DirectionalLightBundle, Mesh, PbrBundle, PerspectiveProjection, ResMut, StandardMaterial,
+        Transform, Vec3,
+    },
+    render::camera::Projection,
+    DefaultPlugins,
+};
 use multiplayer_client::shared::MultiplayerConfig;
 
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
     app.insert_resource(MultiplayerConfig::new(60));
+    app.add_system(flycam::flycam_movement);
+    app.add_startup_system(setup_scene);
     app.run();
+}
+
+fn setup_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(Cube::new(1.0))),
+        material: materials.add(StandardMaterial {
+            base_color: Color::hex("1e2227").unwrap(),
+            perceptual_roughness: 0.35,
+            metallic: 0.5,
+            ..Default::default()
+        }),
+        ..Default::default()
+    });
+
+    commands.spawn_bundle(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            color: Color::hex("ffffff").unwrap(),
+            shadows_enabled: true,
+            illuminance: 10000.0,
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(0.0, 20.0, 0.0),
+        ..Default::default()
+    });
+
+    commands.spawn_bundle(Camera3dBundle {
+        projection: Projection::Perspective(PerspectiveProjection {
+            near: 0.1,
+            far: 1000.0,
+            fov: 45.0,
+            ..Default::default()
+        }),
+        transform: Transform::from_xyz(0.0, 0.5, -10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
 }
